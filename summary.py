1ab1d4f83776da019cebc6f97a3d9c7abd5ed8f6
Bubbliiiing <3323290568@qq.com>
2022-05-01 13:25:16 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -1,11 +1,26 @@
+#--------------------------------------------#
+#   该部分代码用于看网络结构
+#--------------------------------------------#
 from nets.yolo import yolo_body
+from utils.utils import net_flops
 
 if __name__ == "__main__":
-    input_shape = [640, 640, 3]
+    input_shape = [640, 640]
     num_classes = 80
+    phi         = 'l'
 
-    model = yolo_body(input_shape, num_classes, 's')
-
+    model = yolo_body([input_shape[0], input_shape[1], 3], num_classes, phi)
+    #--------------------------------------------#
+    #   查看网络结构网络结构
+    #--------------------------------------------#
     model.summary()
-    for i in range(len(model.layers)): 
-        print(i, model.layers[i].name)
+    #--------------------------------------------#
+    #   计算网络的FLOPS
+    #--------------------------------------------#
+    net_flops(model, table=False)
+    
+    #--------------------------------------------#
+    #   获得网络每个层的名称与序号
+    #--------------------------------------------#
+    # for i,layer in enumerate(model.layers):
+    #     print(i,layer.name)
